040d973296b83f3d790387034a3c8f06f781d3ac
Glassnose <rollinsasia@yahoo.com>
2024-03-27 18:41:25 -0700

    late work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,44 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     private Rigidbody rbPlayer;
     private Vector3 direction = Vector3.zero;
     public float speed = 10.0f;
-    public GameObject spawnPoint = null;
-    private Dictionary<Item.VegetableType, int> ItemInvetory = new Dictionary<Item.VegetableType, int>();
+    public GameObject[] spawnPoints = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         rbPlayer = GetComponent<Rigidbody>();
+        spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
         
-        foreach(Item.VegetableType item in System.Enum.GetValues(typeof(Item.VegetableType)))
-        {
-            ItemInvetory.Add(item, 0);
-        }
     }
 
-    private void AddToInventory(Item item)
-    {
-        ItemInvetory[item.typeOfVeggie]++;
-    }
-
-    private void PrintInventory()
-    {
-        string output = "";
-
-        foreach(KeyValuePair<Item.VegetableType, int> kvp in ItemInvetory)
-        {
-            output += string.Format("{0}: {1} ", kvp.Key, kvp.Value);
-        }
-        Debug.Log(output);
-    }
 
     private void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         float horMove = Input.GetAxis("Horizontal");
         float verMove = Input.GetAxis("Vertical");
 
@@ -48,6 +40,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         rbPlayer.AddForce(direction * speed, ForceMode.Force);
 
         if(transform.position.z > 40)
@@ -62,21 +59,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void Respawn()
     {
-        rbPlayer.MovePosition(spawnPoint.transform.position);
+        int index = 0;
+        while (Physics.CheckBox(spawnPoints[index].transform.position, new Vector3(1.5f, 1.5f, 1.5f)))
+        {
+            index++;
+        }
+        rbPlayer.MovePosition(spawnPoints[index].transform.position);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Item"))
-        {
-            Item item = other.gameObject.GetComponent<Item>();
-            AddToInventory(item);
-            PrintInventory();
-        }
-    }
 
     private void OnTriggerExit(Collider other)
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         if (other.CompareTag("Hazard"))
         {
             Respawn();
